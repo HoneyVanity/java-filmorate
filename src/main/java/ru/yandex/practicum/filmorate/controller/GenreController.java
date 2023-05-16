b9d2e8ff36928a273448969bf6ba0f298e3a211b
HoneyVanity <105306067+HoneyVanity@users.noreply.github.com>
2023-05-17 01:52:40 +0300

    codestyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -5,6 +5,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.GenreService;
+
 import javax.validation.constraints.PositiveOrZero;
 import java.util.Collection;
 
@@ -15,14 +16,16 @@ import java.util.Collection;
 public class GenreController {
 
     private final GenreService genreService;
-    
+
     @GetMapping("/{id}")
     public Genre getById(@PositiveOrZero @PathVariable Long id) {
+
         return genreService.getById(id);
     }
 
     @GetMapping
     public Collection<Genre> getGenres() {
+
         return genreService.getGenres();
     }
 }
